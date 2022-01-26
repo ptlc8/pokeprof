18dc4bfec3af5f45b2c9608e842c5d646678ccd1
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-01-26 14:37:05 +0100

    tournament.php

---
+++ tournament.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<script src="cards.js"></script>
+		<script src="tournament.js"></script>
+		<link rel="stylesheet" href="tournament.css" />
+		<link rel="icon" href="assets/icon.png" />
+	</head>
+	<body>
+		<div id="tournament"></div>
+		<script>
+			displayTournament(document.getElementById("tournament"), "Léo.z.e.r.t.yoyo.u.i.o.Jules.q.Brayan,Léo.2.3.4.Jules.Brayan.pere noel.dealer,Léo.2.Jules.4,Léo.Jules,Jules")
+			
+		</script>
+	</body>
+</html>
