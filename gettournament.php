c0d2c3b82c5bdd240d1a71ebf5a2c3fdd91c3270
LeoLAIOLO <96583365+LeoLAIOLO@users.noreply.github.com>
2022-01-28 22:43:46 +0100

    Plus de modification de la BDD

--- gettournament.php
+++ gettournament.php
@@ -123,8 +123,6 @@ $tournament = $result->fetch_assoc();
 
 $tournament['fighters']=prettyTable4Tournament($tournament['fighters']);
 
-sendRequest("UPDATE TOURNAMENT SET fighters='", $tournament['fighters'] ,"' WHERE id = '", $tournamentId, "'");
-
 echo json_encode($tournament);
 
 ?>
