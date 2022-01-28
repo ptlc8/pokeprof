31a36c2c14b05d8c14706a954a6c6e03763702de
LeoLAIOLO <96583365+LeoLAIOLO@users.noreply.github.com>
2022-01-28 09:49:47 +0100

    Affichage des id des tounois, débogage

--- index.php
+++ index.php
@@ -64,6 +64,7 @@
 			$tournament=array();
 			$lengthTournmnt=0;
 			$tabTourn=$result->fetch_all()[0];
+			print_r($tabTourn);
 			for ($i=0; $i<$result->num_rows; $i++) {
 				$result2=sendRequest("SELECT * FROM TOURNAMENT WHERE id="+$tabTourn[$i]);
 				$tournament[$lengthTournmnt]=$result2->fetc_assoc();
