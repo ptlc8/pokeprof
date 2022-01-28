8dc094d0c6858b306b407364cb6cb409e41e0afb
LeoLAIOLO <96583365+LeoLAIOLO@users.noreply.github.com>
2022-01-28 15:40:51 +0100

    Début récursion création tableau

--- gettournament.php
+++ gettournament.php
@@ -1,6 +1,11 @@
 <?php
 
-//fonction pour faire des tableaux corrects, Léo
+//fonctions pour faire des tableaux corrects, Léo
+
+//function recurPrettyTable ($tree, $nbQualified, $i, $j, $fighters, $indexFighter) {
+	
+//}
+
 function prettyTable4Tournament ($textFighters) {
 	if (($textFighters==null)||(!isset($textFighters))) {
 		return(null);	
@@ -9,14 +14,36 @@ function prettyTable4Tournament ($textFighters) {
 	$trees2=array();
 	$trees3=array();
 	$trees=explode(';',$textFighters);
-	print_r($trees);
 	for ($i=0; $i<count($trees); $i++) {
 		$trees2[$i]=explode(',',$trees[$i]);
-		print_r($trees2);
 		for ($j=0; $j<count($trees2[$i]); $j++) {
 			$trees3[$i][$j]=explode('.',$trees2[$i][$j]);
 		}
 	}
+	//on vérifie si le tournoi a commencé. 2 méthodes:
+	//- regarde les doublons dans le tableau
+	//- existance des repêchages
+	//on choisit la première pour récupérer les participants au passage
+	$i=0;
+	$j=0;
+	$fighters=array();
+	//on s'intéresse seulement au tableau principal
+	while (($i<count($trees3[0]))&&($j==0)) {
+		$k=0;
+		while (($k<count($fighters))&&($k<count($trees3[0][$i]))&&($j==0)) {
+			if ($fighters[$k]==$trees3[0][$i][$k]) {
+				$fighters[$k]=$trees3[0][$i][$k];
+			else {
+				$j=1; //on a un doublon	
+			}
+			$k++;
+		}
+		$i++;
+	}
+	if (j!=0) { //le tournoi a commencé
+		return($trees3);
+	}
+	//modif avec appel récursif
 	return($trees3);
 }
 
