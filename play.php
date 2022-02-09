8e88aaadb283f9f25c98f4e39b1398bfeb541223
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-02-09 14:38:49 +0100

    annulation d'un fix transposé depuis spectate.php

--- play.php
+++ play.php
@@ -486,8 +486,8 @@ if (isset($_REQUEST['error'])) {
 							console.error("[Poképrof] Unknow action name : "+action.name);
 					}
 					refresh();
-					historyIndex++;
 					await sleep(Math.min(500, 5000/(match.history.length-startHistoryIndex)));
+					historyIndex++;
 				}
 				/*if (newActions.length!=0)
 					refresh();*/
