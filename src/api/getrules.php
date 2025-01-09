c5f9bbfd2dd27d003cc3c4693a757f770ad1b61b
Ambi <ptlc.ptlc.ptlc@gmail.com>
2025-01-09 23:44:24 +0100

    Ajout de données manquantes dans les règles

--- src/api/getrules.php
+++ src/api/getrules.php
@@ -1,10 +1,11 @@
 <div style="text-align:left;margin:1em 2em">
     <h1><img class="emote" src="assets/icon.png" /> POKÉPROF : LES RÈGLES</h1>
     <h2><u>But du jeu : DÉGLINGUER TON ADVERSAIRE !</u></h2>
+
     <h2 class="extender" onclick="extendsNext(this)">Comment DÉGLINGUER TON ADVERSAIRE ?</h2>
     <div>
-        Chaque joueur possède un <u style="color:#ff0000">nombre de points de vie</u>, abrégés <b style="color:#ff0000">PV</b> et représentés par <img class="emote" src="assets/heart.svg" alt="un coeur" />. Le but est de faire <span style="color:red">descendre les PV de l'adversaire à zéro ou en deçà</span>.<br/>
-        Pour cela, <u>chaque joueur dispose de <b>cartes</b></u> (<i>oui c’est un jeu de cartes</i>) représentant <b>les héros de <strike>CY Tech</strike> l'EISTI</b>, dont voici les détails ici :
+        Chaque joueur possède un <u style="color:#ff0000">nombre de points de vie</u>, 160 au début d'un combat. Ils sont abrégés <b style="color:#ff0000">PV</b> et représentés par <img class="emote" src="assets/heart.svg" alt="un coeur" />. Le but est de faire <span style="color:red">descendre les PV de l'adversaire à zéro ou en deçà</span>.<br/>
+        Pour cela, <u>chaque joueur dispose de <b>cartes</b></u> (<i>oui c’est un jeu de cartes</i>) représentant <b>les héros de <s>CY Tech</s> l'EISTI</b>, dont voici les détails ici :
     	<h3 class="extender" onclick="extendsNext(this)">Les combattants</h3>
         <div>
     		Ils se battent sur le terrain et ont une ou deux compétences utilisables chaque tour s'ils le peuvent.<br/>
@@ -26,19 +27,23 @@
     	<h3 class="extender" onclick="extendsNext(this)">Les effets</h3>
     	<div>
     		Ces cartes sont à <u>usage unique</u> et ont une compétence propre.<br/>
-    		Que ça soit <i>des actions plus ou moins bénignes</i> ou <i>de simples objets auxquels une part de vie est accrochée</i>, ils ne vous laisseront pas indemnes !
+    		Que ce soit <i>des actions plus ou moins bénignes</i> ou <i>de simples objets auxquels une part de vie est accrochée</i>, ils ne vous laisseront pas indemne !
     	</div>
     	<h3 class="extender" onclick="extendsNext(this)">Les lieux</h3>
     	<div>
-    		C'est le territoire. Permet de définir où la bataille ÉPIQUEUH à lieu. <u>Et peut alors avoir des effets sur vos cartes et celles de l'adversaire</u>.<br/>
+    		C'est le territoire. Permet de définir où la bataille ÉPIQUEUH a lieu. <u>Et peut alors avoir des effets sur vos cartes et celles de l'adversaire</u>.<br/>
     		<i>On ne peut être que dans un seul lieu à la fois.</i>
     	</div>
     </div>
+
     <h2 class="extender" onclick="extendsNext(this)">Comment jouer une carte ?</h2>
     <div>
         Quand vient <b>votre tour</b> (<i>parce que c’est un jeu au tour par tour, oui… !</i>), <u>vous pouvez placer vos cartes</u>.<br/>
         <u>Chaque carte, pour être placée, demande un certain nombre de points de <span style="color:#00bbbb">MANA</span></u> représenté par <img class="emote" src="assets/mana.svg" alt="un cristal" />.<br/>
-        Au début du combat, on a peu de points de <span style="color:#00bbbb">MANA</span>, mais <u>on en gagne à chaque tour</u> <i>et certaines cartes permettent d’en gagner encore plus !</i>
+        Au début du combat, on a peu de points de <span style="color:#00bbbb">MANA</span> : 1 pour celui qui commence et 2 pour son adversaire.<br/>
+        À <u>chaque début de tour</u> le nombre de points de <span style="color:#00bbbb">MANA</span> est réinitialisé avec 1 point en plus (1 point au tour 1, puis 2 au tour 2...) sans dépasser 10, <i>et certaines cartes permettent d’en gagner encore plus !</i><br/>
+        <br/>
+        En fonction du type de carte que vous jouez, elle aura un effet différent :
         <ul>
             <li>Si c'est une carte <b>combattant</b>, elle sera <b>placée sur le terrain</b> et il faudra attendre un tour pour pouvoir attaquer avec (hors éventuelle compétence).</li>
             <li>Si c'est une carte <b>effet</b>, son effet sera <b>appliqué</b> et elle sera défaussée / mise au cimetière.</li>
@@ -46,35 +51,64 @@
         </ul>
         <u>À la fin de votre tour vous pourrez piocher une carte</u>, mais si vous n'avez plus de cartes vous piocherez <span style="color:#ff0000">10 de vos points de vie</span>.
     </div>
+
     <h2 class="extender" onclick="extendsNext(this)">Comment attaquer ?</h2>
     <div>
         <span style="color:#00b050">Une carte invoquée ne peut attaquer qu’au tour suivant son invocation</span><br/>
         Pour attaquer, sélectionnez <b>la carte</b>, puis <b>l’attaque</b> que vous voulez employer, puis éventuellement ce que vous voulez attaquer.<br/>
         Lorsque vous souhaitez attaquer, deux cibles sont possibles : <b>les PV du joueur adverse</b>, ou <b>les cartes qui défendent</b> ce même joueur.<br/>
         CEPENDANT, <span style="color:#ff0000">il est impossible d’attaquer les PV du joueur adverse tant que celui-ci a encore des cartes combattants.</span><br/>
-        Pour éliminer une carte combattant, il faut <b>faire tomber ses PV à zéro ou en deçà</b>.
-        <h3 class="extender" onclick="extendsNext(this)">Comment rendre une carte incapacitante ?</h3>
-        <div>
-            Certaines attaques de vos cartes ont des <b>effets</b> sur les cartes adverses. Ou même sur vos propres cartes.<br/>
-            Cela peut-être un <u>bonus de dégâts sur un certain <font color="#0070c0">t</font><font color="#ff0000">y</font><font color="#DDDD00">p</font><font color="#92d050">e</font> de carte</u>, ou un <u>effet incapacitant</u> (<i>quelle surprise… !</i>).<br/>
-            Parmi les <b>effets incapacitants</b>, il y a :
-            <ul>
-                <li>Le <u>sommeil</u> : endort votre carte et l’<b>empêche d’attaquer</b> jusqu’à ce qu’elle se réveille. Symbolisé par des petits <font color="#00b0f0">z<sup>z</sup><sub>z</sub></font> : 💤.</li>
-                <li>L’<u>apeurement</u> : votre carte CHOK É DÉCU <b>ne pourra pas attaquer</b>. Symbolisé par un <font color="#92d050">smiley CHOK É DÉCU</font> : 😱.</li>
-                <li>
-                    La <u>paralysie</u> : <i>dans Pokémon quand ton Pokémon est paralysé parfois il peut attaquer parce que Magie de Noël et parfois il n’attaque pas parce que paralysie blaireau.<br/>
-                    Le truc, c’est que les cartes de Poképrofs représentent des humains.</i><br/>
-                    Et un humain paralysé ça <b>n’attaque pas</b>.
-                    <i>La vie réelle c’est quand même chiant…</i><br/>
-                    La paralysie finit par disparaître à un moment. Représentée par un panneau de stationnement <font color="#ff0000">interdit</font> 🚫, ce qui est ironique.
-                    <i>RIGOLEUH.</i>
-                </li>
-                <li>L’<u>électrisation</u> : votre carte toujours branchée pète les plombs et <b>perd 10 PV à chaque tour</b>. Symbolisé par un <font color="#DDDD00">éclair</font> <strike>à la vanille</strike> ⚡</li>
-            </ul>
-        </div>
+        Pour éliminer une carte combattant, il faut <b>faire tomber ses PV à zéro ou en deçà</b>.<br/>
+        <br/>
         Vous pouvez terminer votre tour quand vous le souhaitez ; il suffit pour cela de cliquer sur le bouton <u style="color:#DDDD00">« Fin du tour »</u>.<br/>
-        Une carte ayant perdu tous ses <span style="color:#bb0000">points de vie</span> est mise au cimetière, à gauche de la zone de jeu, et <i>outre le fait que j’espère que vous avez réfléchi à un beau discours pour son enterrement</i>, la carte ne pourra pas revenir au jeu <i>sauf si une carte vous en donnent la capacité</i>.
+        Une carte ayant perdu tous ses <span style="color:#bb0000">points de vie</span> est mise au cimetière, à gauche de la zone de jeu, et <i>outre le fait que j’espère que vous avez réfléchi à un beau discours pour son enterrement</i>, la carte ne pourra pas revenir au jeu <i>sauf si une carte vous en donne la capacité</i>.
     </div>
+
+    <h2 class="extender" onclick="extendsNext(this)">Effets malus et bonus ?</h2>
+    <div>
+        Certaines attaques de vos cartes ont des <b>effets</b> sur les cartes adverses. Ou même sur vos propres cartes.<br/>
+        Cela peut-être un <u>bonus de dégâts sur un certain <span style="color:#0070c0">t</span><span style="color:#ff0000">y</span><span style="color:#DDDD00">p</span><span style="color:#92d050">e</span> de carte</u>, un <u>bonus</u> ou un <u>malus</u> (<i>quelle surprise… !</i>).<br/>
+        Parmi les <b>malus</b>, il y a :
+        <ul>
+            <li>
+                Le <u>sommeil</u> : endort votre carte et l’<b>empêche d’attaquer</b> jusqu’à ce qu’elle se réveille.
+                Symbolisé par des petits <span style="color:#00b0f0">z<sup>z</sup><sub>z</sub></span> : 💤.
+            </li>
+            <li>
+                L’<u>apeurement</u> : votre carte CHOK É DÉCU <b>ne pourra pas attaquer</b>.
+                Symbolisé par un <span style="color:#92d050">smiley CHOK É DÉCU</span> : 😱.
+            </li>
+            <li>
+                La <u>paralysie</u> : <i>dans Pokémon quand ton Pokémon est paralysé parfois il peut attaquer parce que Magie de Noël et parfois il n’attaque pas parce que paralysie blaireau.<br/>
+                Le truc, c’est que les cartes de Poképrofs représentent des humains.</i><br/>
+                Et un humain paralysé ça <b>n’attaque pas</b>. <i>La vie réelle c’est quand même chiant…</i><br/>
+                La paralysie finit par disparaître à un moment.
+                Représentée par un panneau de stationnement <span style="color:#ff0000">interdit</span> 🚫, ce qui est ironique.
+                <i>RIGOLEUH.</i>
+            </li>
+            <li>
+                L’<u>électrisation</u> : votre carte toujours branchée pète les plombs et <b>perd 10 PV à chaque fin de votre tour</b>.
+                Symbolisée par un <span style="color:#DDDD00">éclair</span> <s>à la vanille</s> ⚡.
+            </li>
+        </ul>
+        Et parmi les <b>bonus</b> :
+        <ul>
+            <li>
+                Le <u>bouclier</u> : protège votre carte en <b>absorbant les dégâts reçus</b>.
+                Un bouclier absorbant X dégâts diminuera sa valeur de X et les dégâts restants seront infligés à la carte.
+                Symbolisé par un <span style="color:#888888">bouclier</span> <img class="emote" src="assets/shield.svg" alt="un bouclier" />.
+            </li>
+            <li>
+                La <u>force</u> : ajoute des <b>dégâts supplémentaires</b> aux attaques de votre carte.
+                Symbolisée par un <span style="color:#B6864B">poing</span> <img class="emote" src="assets/fist.svg" alt="un poing" />.
+            </li>
+            <li>
+                La <u>provocation</u> : oblige les cartes adverses à attaquer cette carte en priorité.
+                Symbolisée par un emoji qui dit des <span style="color:#DD6600">grossièretés</span> : 🤬.
+            </li>
+        </ul>
+    </div>
+
     <h2 class="extender" onclick="extendsNext(this)">Comment former un deck ?</h2>
     <div>
         Le <b>deck</b>, votre <b>deck</b>, contient toutes vos cartes lors d'un match. Au maximum, il en contient <span style="color:#0070c0">20</span>, et parmi elles <span style="color:#ffc000">pas plus de 3 cartes identiques</span>.<br/>
